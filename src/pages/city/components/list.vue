3179ab5adb8c4321567e02701d82b3ddc39f1b15
tuyyang <46390022+tuyyang@users.noreply.github.com>
2019-12-23 21:38:27 +0800

    优化list

--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -40,10 +40,19 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted() {
     this.scroll=new BScroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter() {
+      if(this.letter) {
+        const element=this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
