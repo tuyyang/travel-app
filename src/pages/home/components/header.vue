5ab0ed7b845f0caf3680b0376ff6ac54ce1ac688
tuyyang <46390022+tuyyang@users.noreply.github.com>
2019-12-23 15:43:27 +0800

    finish header

---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      {{ this.city }}
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  props: {
+    city: String
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  //1rem = html font-size = 50px
+  @import '~styles/varibles.styl'
+
+  .header
+    display flex
+    height 0.86rem
+    background $bgColor
+    color #fff
+    .header-left
+      width 0.64rem
+      float left
+      .back-icon
+        width 100%
+        height 100%
+        margin-top 0.22rem
+        text-align center
+        font-size 0.4rem
+        justify-content center
+        align-items center
+    .header-input
+      flex 1
+      height 0.64rem
+      line-height 0.64rem
+      margin-top 0.12rem
+      margin-left 0.2rem
+      background #fff
+      color #ccc
+      border-radius 0.1rem
+    .header-right
+      width 1.24rem
+      float right
+      text-align center
+      margin-top 0.25rem
+      .arrow-icon
+        margin-left -0.06rem
+        font-size 0.24rem
+</style>
---
+++ src/pages/home/home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <home-header :city="city"></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './components/header'
+import HomeSwiper from './components/swiper'
+import HomeIcons from './components/icons'
+import HomeRecommend from './components/recommend'
+import HomeWeekend from './components/weekend'
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
+  },
+  data() {
+    return {
+      city: '',
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  methods: {
+    getHomeInfo() {
+      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc(res) {
+      res=res.data
+      if(res.ret&&res.data) {
+        const data=res.data
+        this.city=data.city
+        this.swiperList=data.swiperList
+        this.iconList=data.iconList
+        this.recommendList=data.recommendList
+        this.weekendList=data.weekendList
+      }
+      console.log(res)
+    }
+  },
+  mounted() {
+    this.getHomeInfo()
+  }
+}
+</script>
+
+<style lang='stylus'></style>
