cfd19ecb70e766f59057f61b28d0873d21553d5d
tuyyang <46390022+tuyyang@users.noreply.github.com>
2019-12-23 20:26:29 +0800

    finish city-list

---
+++ src/pages/city/components/alphabet.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="list">
+    <li class="item"></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/baribles.styl'
+
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width 0.4rem
+    .item
+      line-height 0.4rem
+      text-align center
+      background $bgColor
+</style>
