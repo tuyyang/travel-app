6196fe60259fe8a44736f78a141de246b827e56b
tuyyang <46390022+tuyyang@users.noreply.github.com>
2019-12-24 16:52:40 +0800

    finish detail-banner

---
+++ src/pages/detail/components/banner.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img
+        src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg"
+        class="banner-img"
+      />
+      <div class="banner-info">
+        <div class="banner-title"></div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe692;</span>
+        </div>
+      </div>
+    </div>
+    <common-gallery
+      :imgs="imgs"
+      v-show="showGallery"
+      @close="handleGalleryClose"
+    ></common-gallery>
+  </div>
+</template>
+
+<script>
+import CommonGallery from 'common/gallery/gallery'
+
+export default {
+  name: 'DetailBanner',
+  data() {
+    return {
+      showGallery: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg', 'http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png']
+    }
+  },
+  methods: {
+    handleBannerClick() {
+      this.showGallery=true
+    },
+    handleGalleryClose() {
+      this.showGallery=false
+    }
+  },
+  components: {
+    CommonGallery
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 50%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height 0.6rem
+      background-image linear-gradient(to bottom,rgba(0,0,0,0),rgba(0,0,0,0.8))
+      .banner-title
+        flex 1
+        font-size 0.32rem
+        padding 0.2rem
+      .banner-number
+        padding 0 0.4rem
+        height 0.32rem
+        line-height 0.32rem
+        margin-top 0.14rem
+        font-size 0.24rem
+        border-radius 0.2rem
+        background rgba(0,0,0,0.8)
+        color #fff
+        .banner-icon
+          font-size 0.24rem
+</style>
